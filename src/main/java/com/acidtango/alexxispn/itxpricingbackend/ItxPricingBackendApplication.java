150b658999035011b3cedf1fa7a8354fec8dcdd2
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-19 22:53:59 +0100

    🎉 initial commit

---
+++ src/main/java/com/acidtango/alexxispn/itxpricingbackend/ItxPricingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acidtango.alexxispn.itxpricingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItxPricingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItxPricingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acidtango/alexxispn/itxpricingbackend/ItxPricingBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.acidtango.alexxispn.itxpricingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItxPricingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
