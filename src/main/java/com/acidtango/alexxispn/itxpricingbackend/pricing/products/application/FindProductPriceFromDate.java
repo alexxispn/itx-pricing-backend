6f62ffa17638aa1568ec4ea7e81e1ac75aee4a91
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-20 10:44:36 +0100

    🟢green: not find a product price

--- src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/application/FindProductPriceFromDate.java
+++ src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/application/FindProductPriceFromDate.java
@@ -16,14 +16,7 @@ public class FindProductPriceFromDate extends UseCase {
         this.repository = repository;
     }
 
-    public ProductPrice execute(String brandCode, String productCode, LocalDateTime dateTime) {
-        return ProductPrice.create(
-                new ProductPriceId(UUID.randomUUID().toString()),
-                new ProductCode(productCode),
-                new BrandCode(brandCode),
-                new RangeDateTime(LocalDateTime.of(2020, 6, 14, 0, 0, 0), LocalDateTime.of(2020, 12, 31, 23, 59, 59)),
-                new Price(35.50, "EUR"),
-                new Priority(0)
-        );
+    public ProductPriceReadModel execute(String brandCode, String productCode, LocalDateTime dateTime) {
+        return repository.find(productCode, brandCode, dateTime.toString());
     }
 }
