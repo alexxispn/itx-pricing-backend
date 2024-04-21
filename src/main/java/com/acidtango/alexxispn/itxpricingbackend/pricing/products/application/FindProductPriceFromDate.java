e828b0b4123da3f65f3be4605063f28d13a413e0
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-21 14:25:04 +0100

    🔵refactor: replace with 'Comparator.comparingInt'

--- src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/application/FindProductPriceFromDate.java
+++ src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/application/FindProductPriceFromDate.java
@@ -7,6 +7,7 @@ import com.acidtango.alexxispn.itxpricingbackend.pricing.shared.application.UseC
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +23,7 @@ public class FindProductPriceFromDate extends UseCase {
     public Optional<ProductPriceResponseDto> execute(String brandCode, String productCode, Instant dateTime) {
         List<ProductPrice> productPrices = repository.find(productCode, brandCode, dateTime);
         Optional<ProductPrice> productPriceWithHighestPriority =
-                productPrices.stream().max((p1, p2) -> Integer.compare(p1.priority(), p2.priority()));
+                productPrices.stream().max(Comparator.comparingInt(ProductPrice::priority));
         return productPriceWithHighestPriority.map(productPrice -> new ProductPriceResponseDto(
                 productPrice.toPrimitives().productCode(),
                 productPrice.toPrimitives().brandCode(),
