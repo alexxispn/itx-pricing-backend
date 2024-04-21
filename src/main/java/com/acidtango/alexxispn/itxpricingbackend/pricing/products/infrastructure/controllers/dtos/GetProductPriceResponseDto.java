fe598b2b2b179011c3abc87d8c9d4d0fc10e0f1c
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-21 14:23:43 +0100

    🔵refactor: safe delate from domain in dto

--- src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/infrastructure/controllers/dtos/GetProductPriceResponseDto.java
+++ src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/infrastructure/controllers/dtos/GetProductPriceResponseDto.java
@@ -1,24 +1,11 @@
 package com.acidtango.alexxispn.itxpricingbackend.pricing.products.infrastructure.controllers.dtos;
 
-import com.acidtango.alexxispn.itxpricingbackend.pricing.products.domain.ProductPrice;
-
 import java.time.Instant;
 
 public record GetProductPriceResponseDto(
         String productCode, String brandCode, Instant fromDateTime, Instant toDateTime, double amount,
         String currencyCode
 ) {
-    public static GetProductPriceResponseDto fromDomain(ProductPrice productPrice) {
-        var primitives = productPrice.toPrimitives();
-        return new GetProductPriceResponseDto(
-                primitives.productCode(),
-                primitives.brandCode(),
-                primitives.fromDateTime(),
-                primitives.toDateTime(),
-                primitives.amount(),
-                primitives.currencyCode()
-        );
-    }
 
     public ProductPriceResponseDto productPrice() {
         return new ProductPriceResponseDto(
