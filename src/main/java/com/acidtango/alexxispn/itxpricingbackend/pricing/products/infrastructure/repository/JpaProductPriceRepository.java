31bb9f943dbfff858c794b1f7da966a490d50b83
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-21 14:17:09 +0100

    🟢green: invoke seeder on initialization

--- src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/infrastructure/repository/JpaProductPriceRepository.java
+++ src/main/java/com/acidtango/alexxispn/itxpricingbackend/pricing/products/infrastructure/repository/JpaProductPriceRepository.java
@@ -3,5 +3,14 @@ package com.acidtango.alexxispn.itxpricingbackend.pricing.products.infrastructur
 import com.acidtango.alexxispn.itxpricingbackend.pricing.products.infrastructure.repository.entities.ProductPriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaProductPriceRepository extends JpaRepository<ProductPriceEntity,
+import java.time.Instant;
+import java.util.List;
+
+
+public interface JpaProductPriceRepository extends JpaRepository<ProductPriceEntity, String> {
+    List<ProductPriceEntity> findByProductCodeAndBrandCodeAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
+            String productCode,
+            String brandCode,
+            Instant startDate,
+            Instant endDate);
 }
